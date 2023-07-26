33e162cec22acc3cf016f9be3d63e0e1d7dffcb4
VishnevetskayaSasha <visneveckaya.a@gmail.com>
2023-07-26 15:22:02 +0300

    layout + js

---
+++ controllable-table/index.js
@@ -0,0 +1,24 @@
+let buttonUp = document.querySelector(".main__button-up");
+let buttonDown = document.querySelector(".main__button-down");
+let imgPart1 = document.querySelector(".main__img-part1");
+let imgPart2 = document.querySelector(".main__img-part2");
+
+buttonUp.addEventListener("click", function(){
+    
+    buttonUp.setAttribute('disabled', '');
+    buttonDown.disabled = false;
+    if (window.innerWidth > 850){
+        imgPart1.style.top = '-80px';
+    } else {
+        imgPart1.style.top = '-40px';
+    }
+})
+
+
+buttonDown.addEventListener('click', function() {
+    imgPart1.style.top = '0';
+    buttonUp.disabled = false;
+    buttonDown.disabled = true;
+  });
+
+
